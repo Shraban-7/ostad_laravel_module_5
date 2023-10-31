0073d2f97ae533670b9c9fe3eac81f0efc3b230c
Shraban-7 <shakuatshraban@gmail.com>
2023-10-31 23:08:02 +0600

    first commit

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once("./header/header.php");
+require_once("./functions.php");
+
+// Check if the user is not authenticated
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: ' . BASE_URL . "/login/index.php");
+    exit;
+}
+
+// Check if the user is an admin
+if (isAdmin()) {
+    header('Location: ' . BASE_URL . "/dashboard/index.php");
+    exit;
+}
+
+// At this point, the user is not an admin and not authenticated
+
+?>
+<!-- <section class="vh-100" style="background-color: #eee;"> -->
+<section class="vh-100 parallaxsss">
+    <div class="container parallaxsss">
+        <div class="row parallaxsss">
+            <div class="col-lg-12 mt-5 parallaxsss">
+                <?php
+                if (isset($_SESSION['username'])) {
+                    echo "Welcome, " . htmlspecialchars($_SESSION['username']) . "! You are now logged in.";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once("./header/footer.php"); ?>
